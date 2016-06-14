befef4747e0a81c60ba28c9c19345c98aa549080
jun.liu.neu <jun.liu.neu@neusoft.com>
2016-06-14 16:35:15 +0800

    init

---
+++ src/test/java/com/jim/captcha/CaptchaApplicationTests.java
@@ -0,0 +1,18 @@
+package com.jim.captcha;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CaptchaApplication.class)
+@WebAppConfiguration
+public class CaptchaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
